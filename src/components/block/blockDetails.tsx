d48b8f71be8dc53a02ad0d616f0472252c85e484
qperrot <quentinperrotm@gmail.com>
2024-07-19 14:41:03 +0200

    refacto: creat a reusable components to display block info and removed unused import

--- src/components/block/blockDetails.tsx
+++ src/components/block/blockDetails.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { RpcProvider } from 'starknet-6';
 import { Box, Button, Divider, Grid, Stack, Typography } from '@mui/material';
 import { useSharedState } from '../context/context';
@@ -6,6 +6,7 @@ import { useNavigate, useParams } from 'react-router-dom';
 import { ChevronLeft } from '@mui/icons-material';
 import { shortenAddress } from '../utils/utils';
 import { darkTheme } from '../..';
+import DisplayBlockInfo from './displayBlockInfo';
 
 const BlockDetailsPage: React.FC = () => {
   const context = useSharedState();
@@ -14,25 +15,26 @@ const BlockDetailsPage: React.FC = () => {
   const { blockIndex } = useParams<{ blockIndex: string }>();
   const index = parseInt(blockIndex || '', 10);
 
-  async function fetchTransactionsCountByBlock() {
+  async function fetchTransactionsDetailsByBlock() {
     const provider = new RpcProvider({ nodeUrl: `${url}/rpc` });
     const tx = await provider.getBlockWithTxs(index);
     setBlockDetails(tx);
   }
 
   const handleBack = () => {
+    // TODO: Update to navigate to the second tab on the home screen where the blocks are.
     navigate('/');
   };
 
   useEffect(() => {
-    const fetchTransactionsCount = async () => {
+    const fetchTransactionsDetails = async () => {
       try {
-        await fetchTransactionsCountByBlock();
+        await fetchTransactionsDetailsByBlock();
       } catch (error) {
         console.error('Error fetching current block number:', error);
       }
     };
-    fetchTransactionsCount();
+    fetchTransactionsDetails();
   }, []);
 
   return (
@@ -53,51 +55,31 @@ const BlockDetailsPage: React.FC = () => {
         </Box>
       </Stack>
       <Box padding={1} flexGrow={1}>
-        <Stack direction={'row'} spacing={{ xs: 3, sm: 2 }} useFlexGap flexWrap="wrap">
-          <Stack alignItems={'flex-start'}>
-            <Typography color={darkTheme.palette.text.secondary} variant="caption">
-              Block Number
-            </Typography>
-            <Stack direction={'row'} alignItems={'center'} spacing={1}>
-              <Typography fontSize={'0.9rem'}>{blockDetails.block_number}</Typography>
-            </Stack>
-          </Stack>
-          <Stack alignItems={'flex-start'}>
-            <Typography color={darkTheme.palette.text.secondary} variant="caption">
-              Block Hash
-            </Typography>
-            <Stack direction={'row'} alignItems={'center'} spacing={1}>
-              <Typography fontSize={'0.9rem'}>
-                {shortenAddress(blockDetails.block_hash, 5)}
-              </Typography>
-            </Stack>
-          </Stack>
-          <Stack alignItems={'flex-start'}>
-            <Typography color={darkTheme.palette.text.secondary} variant="caption">
-              Timestamp
-            </Typography>
-            <Stack direction={'row'} alignItems={'center'} spacing={1}>
-              <Typography fontSize={'0.9rem'}>{blockDetails.timestamp ?? 'Unknown'}</Typography>
-            </Stack>
-          </Stack>
-          <Stack alignItems={'flex-start'}>
-            <Typography color={darkTheme.palette.text.secondary} variant="caption">
-              Starknet Version
-            </Typography>
-            <Stack direction={'row'} alignItems={'center'} spacing={1}>
-              <Typography fontSize={'0.9rem'}>
-                {blockDetails.starknet_version ?? 'Unknown'}
-              </Typography>
-            </Stack>
-          </Stack>
-          <Stack alignItems={'flex-start'}>
-            <Typography color={darkTheme.palette.text.secondary} variant="caption">
-              Status
-            </Typography>
-            <Stack direction={'row'} alignItems={'center'} spacing={1}>
-              <Typography fontSize={'0.9rem'}>{blockDetails.status ?? 'Unknown'}</Typography>
-            </Stack>
-          </Stack>
+        <Stack direction={'row'} spacing={{ xs: 2, sm: 2 }} useFlexGap flexWrap="wrap">
+          <DisplayBlockInfo
+            title="Block Number"
+            value={blockDetails.block_number}
+          ></DisplayBlockInfo>
+          <DisplayBlockInfo
+            title="Block Hash"
+            value={shortenAddress(blockDetails.block_hash, 5)}
+          ></DisplayBlockInfo>
+          <DisplayBlockInfo
+            title="Timestamp"
+            value={blockDetails.timestamp ?? 'Unknown'}
+          ></DisplayBlockInfo>
+          <DisplayBlockInfo
+            title="Starknet Version"
+            value={blockDetails.starknet_version ?? 'Unknown'}
+          ></DisplayBlockInfo>
+          <DisplayBlockInfo
+            title="Transactions"
+            value={blockDetails.transactions ? blockDetails.transactions.length : '0'}
+          ></DisplayBlockInfo>
+          <DisplayBlockInfo
+            title="Status"
+            value={blockDetails.status ?? 'Unknown'}
+          ></DisplayBlockInfo>
         </Stack>
         {blockDetails.transactions && blockDetails.transactions.length > 0 && (
           <>
@@ -109,42 +91,19 @@ const BlockDetailsPage: React.FC = () => {
             </Stack>
             {blockDetails.transactions.map((info: any, index: number) => (
               <Stack direction={'row'} spacing={{ xs: 2, sm: 2 }} useFlexGap flexWrap="wrap">
-                <Stack alignItems={'flex-start'}>
-                  <Typography color={darkTheme.palette.text.secondary} variant="caption">
-                    Hash
-                  </Typography>
-                  <Stack direction={'row'} alignItems={'center'} spacing={1}>
-                    <Typography fontSize={'0.9rem'}>
-                      {shortenAddress(info.transaction_hash, 5)}
-                    </Typography>
-                  </Stack>
-                </Stack>
-                <Stack alignItems={'flex-start'}>
-                  <Typography color={darkTheme.palette.text.secondary} variant="caption">
-                    Sender
-                  </Typography>
-                  <Stack direction={'row'} alignItems={'center'} spacing={1}>
-                    <Typography fontSize={'0.9rem'}>
-                      {shortenAddress(info.sender_address, 5)}
-                    </Typography>
-                  </Stack>
-                </Stack>
-                <Stack alignItems={'flex-start'}>
-                  <Typography color={darkTheme.palette.text.secondary} variant="caption">
-                    Nonce
-                  </Typography>
-                  <Stack direction={'row'} alignItems={'center'} spacing={1}>
-                    <Typography fontSize={'0.9rem'}>{info.nonce}</Typography>
-                  </Stack>
-                </Stack>
-                <Stack alignItems={'flex-start'}>
-                  <Typography color={darkTheme.palette.text.secondary} variant="caption">
-                    Max Fee
-                  </Typography>
-                  <Stack direction={'row'} alignItems={'center'} spacing={1}>
-                    <Typography fontSize={'0.9rem'}>{parseInt(info.max_fee || '', 16)}</Typography>
-                  </Stack>
-                </Stack>
+                <DisplayBlockInfo
+                  title="Hash"
+                  value={shortenAddress(info.transaction_hash, 5)}
+                ></DisplayBlockInfo>
+                <DisplayBlockInfo
+                  title="Sender"
+                  value={shortenAddress(info.sender_address, 5)}
+                ></DisplayBlockInfo>
+                <DisplayBlockInfo title="Nonce" value={info.nonce}></DisplayBlockInfo>
+                <DisplayBlockInfo
+                  title="Max Fee"
+                  value={parseInt(info.max_fee || '', 16)}
+                ></DisplayBlockInfo>
                 {blockDetails.length - 1 !== index ? <Divider variant="middle" /> : null}
               </Stack>
             ))}
