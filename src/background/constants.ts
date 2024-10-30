bceb6d78a55772e74f7cf9f74da9edcef4451d30
Darko Sperac <dsperac@gmail.com>
2024-10-30 09:10:54 +0100

    added button to add default localhost devnet url if not in list

--- src/background/constants.ts
+++ src/background/constants.ts
@@ -1,4 +1,5 @@
 export const DEFAULT_DEVNET_URL = 'https://devnet.spaceshard.io';
+export const LOCALHOST_DEVNET_URL = 'http://127.0.0.1:5050';
 export const ETH_ADDRESS = '0x49d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7';
 export const MAX_AMOUNT_TO_MINT = 100000000000000000000n;
 
